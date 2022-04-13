3f941b8e268251c9668dc72e61f94c5e1f6347a0
mikekondo <foad5969@gmail.com>
2022-04-13 16:52:42 +0900

    画面1の編集

--- Github-revertTest/ViewController.swift
+++ Github-revertTest/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tappedNextButton(_ sender: Any) {
+    }
 
 }
 
